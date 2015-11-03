ec614830bbafe3ab2df14257e7bf3bb09292cc73
Karissa McKelvey <krmckelv@gmail.com>
2015-11-02 16:09:07 -0800

    abstract out searcher

--- bin/collect.js
+++ bin/collect.js
@@ -1,19 +1,18 @@
 var from = require('from2')
 var pump = require('pump')
 var through = require('through2')
-var Twitter = require('twitter')
 var debug = require('debug')('theinternetthing')
 
+var search = require('./search.js')
 var convert = require('./convert.js')
 var server = require('../server/server.js')
-var rate = require('./rate.js')
 
-var client = new Twitter({
+var client = {
   consumer_key: process.env.TWITTER_KEY,
   consumer_secret: process.env.TWITTER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_SECRET
-})
+}
 
 var Query = server.models.Query
 Query.find({ where: { running: 'true' } }, function (err, queries) {
@@ -24,49 +23,16 @@ Query.find({ where: { running: 'true' } }, function (err, queries) {
     var opts = {
       q: includes.join(',')
     }
-    scrapeSearch(query, opts)
+    var searcher = search(client, opts)
+    searcher.on('data', function (tweets) {
+      var tweetStream = from.obj(tweets)
+      var saveTweets = through.obj(function (data, enc, next) {
+        var tweet = convert(data)
+        query.tweets.create({data: tweet}, next)
+      })
+      pump(tweetStream, saveTweets, function (err) {
+        if (err) console.error('boo', err)
+      })
+    })
   }
 })
-
-function scrapeSearch (query, opts) {
-  debug('waiting for 5 sec')
-  setTimeout(function () {
-    search(query, opts, function (err, since_id) {
-      if (err) console.error(err)
-      opts.since_id = since_id
-      scrapeSearch(query, opts)
-    })
-  }, 5000)
-}
-
-function search (query, opts, cb) {
-  debug('searching for', opts)
-  opts.result_type = 'recent'
-  client.get('search/tweets', opts, function (err, data, response) {
-    if (err) {
-      if (err[0].code === 88) {
-        return rate(client, 'search', function (err, data, response) {
-          if (err) console.error(err)
-          var reset = data.resources.search['/search/tweets'].reset
-          var diff = new Date(reset * 1000) - new Date() + 500
-          debug('waiting for', diff / 1000, 'sec')
-          setTimeout(function () { cb(null, opts.since_id) }, diff)
-        })
-      }
-      return cb(err)
-    }
-    debug('got', data.statuses.length, 'tweets')
-    if (data.statuses.length === 0) return cb(null, opts.since_id)
-
-    var tweetStream = from.obj(data.statuses)
-    var since_id = data.statuses[0].id
-    var saveTweets = through.obj(function (data, enc, next) {
-      var tweet = convert(data)
-      query.tweets.create({data: tweet}, next)
-    })
-    pump(tweetStream, saveTweets, function (err) {
-      if (err) return cb(err)
-      return cb(null, since_id)
-    })
-  })
-}
--- bin/rate.js
+++
@@ -1,3 +0,0 @@
-module.exports = function rate (client, resources, cb) {
-  client.get('application/rate_limit_status', {resources: resources}, cb)
-}
